98263d906f83cfd8bbc72759a4fdc8e75ceb221e
Tcilley <redbrickbarn@pm.me>
2026-04-09 17:52:01 +0000

    Deploying to gh-pages from @ Tcilley/Tcilley.github.io@9319ce18d4cbcf0f7fdd70206a1264abd696ae40 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Tcilley/tcilley.github.io",
+    "data-repo": "Tcilley/Tcilley.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
